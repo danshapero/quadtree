68f6b2486137dfa9945b38521bb4f6f124d8fe05
danshapero <shapero.daniel@gmail.com>
2014-06-29 12:06:25 -0700

    Constructor for quadtree compiles and runs without seg-fault, awaits testing

--- quadtree.hpp
+++ quadtree.hpp
@@ -79,6 +79,62 @@ class QuadTreeCell {                                                      //
         return R;
     }
 
+
+     ///////////
+    // Mutators
+
+    // Set one of the cell's children
+    void setChild(int index, QuadTreeCell<DIM>& cell) {
+        child[index] = &cell;
+    }
+
+    /* Insert a new point `p` into this cell if it has room or into a child
+       if it does not. Note that we do not store the actual point at the
+       cell, just an integer `index` that can be used to identify it in a
+       list of points later. */
+    bool insertPoint(int index, Point<DIM>& p) {
+        if ( ! R.inRectangle(p) ) {
+            return false;
+        }
+
+        /* If there's enough space in this cell's internal storage, store
+           it here */
+        if ( point_count < CAPACITY ) {
+            indices[point_count++] = index;
+            return true;
+        }
+
+        // Otherwise, find the quadrant to which the point belongs
+        Point<DIM> q = getCenter();
+        int k = q.quadrant(p);
+
+        // If the corresponding child cell is not empty, recurse to it
+        if ( child[k] ) {
+            return child[k]->insertPoint(index, p);
+
+        // Otherwise, make a new cell
+        } else {
+            int t;
+            double L = getHalfWidth()/2;
+
+            for (int i = 0; i < DIM; i++) {
+                // t == -1 if the i-th bit of index is 1, 0 otherwise
+                t = 1 - 2*((index & (1<<i)) != 0);
+
+                /* Choose q[i] = this.center[i] +/- this.width/2 according
+                   to which quadrant it belongs to. */
+                q[i] = q[i] + t*L;
+            }
+
+            // Initialize the appropriate child of this cell
+            child[k] = new QuadTreeCell<DIM>(q, L);
+
+            // Insert X[index] into the child cell
+            return child[k]->insertPoint(index, p);
+        }
+
+        return false;
+    }
 };
 
 
@@ -108,10 +164,23 @@ class QuadTree {                                                          //
 
     QuadTree(Rectangle<DIM>& boundingBox, std::vector< Point<DIM> >& P) {
         R = boundingBox;
-        root = NULL;
+        root = new QuadTreeCell<2>(boundingBox);
 
         num_points = P.size();
         X = &P;
+
+        for (int index = 0; index < num_points; index++) {
+            root->insertPoint(index, P[index]);
+        }
+    }
+
+
+     //////////////
+    // Destructors
+
+    ~QuadTree() {
+        delete root;
+        X = NULL;
     }
 
 
@@ -125,4 +194,15 @@ class QuadTree {                                                          //
     Point<DIM> getPoint(int k) {
         return (*X)[k];
     }
+
+
+     ///////////
+    // Mutators
+
+    /*bool insertPoint( Point<DIM>& p ) {
+        QuadTreeCell<2>* cell = root;
+
+        return false;
+    }*/
+
 };
